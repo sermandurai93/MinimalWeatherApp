fcad1f9214d1d3b53c811dc3d3e82571cdfe2801
SermanDurai <sermandurai@Sermandurai-iMac.local>
2017-06-10 12:55:47 +0530

    feat: Initial commit

---
+++ MinimalWeatherApp/MinimalWeatherApp/Constance/MWAConstance.swift
@@ -0,0 +1,9 @@
+//
+//  MWAConstance.swift
+//  MinimalWeatherApp
+//
+//  Created by SermanDurai on 10/06/17.
+//  Copyright © 2017 Sermandurai Subbiah. All rights reserved.
+//
+
+import Foundation
---
+++ MinimalWeatherApp/MinimalWeatherApp/Extensions/UIFont+CustomFont.swift
@@ -0,0 +1,13 @@
+//
+//  UIFont+CustomFont.swift
+//  MinimalWeatherApp
+//
+//  Created by SermanDurai on 10/06/17.
+//  Copyright © 2017 Sermandurai Subbiah. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+}
